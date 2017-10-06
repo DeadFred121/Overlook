40d664ea4ec05746e837b7a42f53bb81e5be8263
DeadFred <f.cliff1@hotmail.com>
2017-10-06 14:07:30 +1000

    rooms moved to db, routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get '/contact' => 'pages#contact'
 
+  get '/about' => 'pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
